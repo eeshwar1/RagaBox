80971d1f5865f0880bb4653e30ce47e716b35a9d
Venky Venkatakrishnan <venkyul.appdev@gmail.com>
2020-12-30 13:04:16 -0600

    Added dropdown for Raga and label to display current Note

--- RagaBox/ViewController.swift
+++ RagaBox/ViewController.swift
@@ -13,42 +13,70 @@ class ViewController: NSViewController, AVAudioPlayerDelegate {
     var players =  [NSURL: AVAudioPlayer]()
     var duplicatePlayers = [AVAudioPlayer] ()
     
-    var abhogi: [String] = ["C3","D3","D#3","F3","A3","C4", "C4","A3","F3","D#3","D3","C3"]
+    let ragaNotes = ["abhogi" : ["C3","D3","D#3","F3","A3","C4", "C4","A3","F3","D#3","D3","C3"],
     
-    var kharaharapriya: [String] = ["C3","D3","D#3","F3","G3","A3","A#3","C4", "C4","A#3","A3","G3","F3","D#3","D3","C3"]
+                  "kharaharapriya":  ["C3","D3","D#3","F3","G3","A3","A#3","C4", "C4","A#3","A3","G3","F3","D#3","D3","C3"],
     
-    var shanmukhapriya: [String] = ["C3","D3","D#3","F#3","G3","G#3","A#3","C4", "C4","A#3","G#3","G3","F#3","D#3","D3","C3"]
+                    "shanmughapriya" :  ["C3","D3","D#3","F#3","G3","G#3","A#3","C4", "C4","A#3","G#3","G3","F#3","D#3","D3","C3"],
     
-    var ahiri: [String] = ["C3","D3","F3","E3","F3","G3","A3","B3","C4", "C4","B3","A3","G3","F3","E3","F3","D3","C3"]
+                    "ahiri":  ["C3","D3","F3","E3","F3","G3","A3","B3","C4", "C4","B3","A3","G3","F3","E3","F3","D3","C3"],
+                    
+                    "reetigowla": ["C3", "E3", "D3", "E3", "F3", "B3", "A3", "F3", "B3", "B3", "C4",
+                        "C4", "B3", "A3", "F3", "E3", "F3", "G3","F3", "E3", "D3", "C3"]]
+    
+    @IBOutlet weak var popupButton: NSPopUpButton!
+    @IBOutlet weak var labelNote: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        popupButton.removeAllItems()
+        popupButton.addItems(withTitles: Array(ragaNotes.keys).sorted())
     }
     
     @IBAction func playSound(_ sender: Any) {
+
         
-        playNotes(notes: abhogi,withDelay: 0.4)
+        let raga = popupButton.title
+       
+        // print("Selected Raga is \(raga)")
+        if let notes = ragaNotes[raga.lowercased()] {
+            // print("Notes are \(String(describing: notes))")
+            playNotes(notes: notes,withDelay: 0.5)
+        }
+        else {
+            print("Notes not defined for raga \(raga)")
+        }
+        
+        self.labelNote.stringValue = ""
    
        
     }
     
     func playNotes(notes: [String], withDelay: Double)
     {
+        var lastDelay = 0.0
         for (index, note) in notes.enumerated() {
             
             // print("Note(\(index)): \(note)")
             let delay = withDelay*Double(index)
+            lastDelay = delay
             let _ = Timer.scheduledTimer(timeInterval: delay, target: self, selector: #selector(playNoteNotification(notification:)), userInfo: ["note":note], repeats: false)
         }
+        lastDelay = lastDelay + withDelay
+        let _ = Timer.scheduledTimer(timeInterval: lastDelay, target: self, selector: #selector(donePlaying(notification:)), userInfo: nil, repeats: false)
+       
     }
     
+    @objc func donePlaying(notification: NSNotification) {
+        
+        self.labelNote.stringValue = ""
+    }
     @objc func playNoteNotification(notification: NSNotification) {
        
         if let note = notification.userInfo?["note"] as? String {
-            
-            // print("Playing sound: \(note)")
+       
+            self.labelNote.stringValue = note
             playNote(note)
             
         }
@@ -103,14 +131,17 @@ class ViewController: NSViewController, AVAudioPlayerDelegate {
                     print("Could not play note: \(note)")
                 }
             }
+        
+        
     }
     
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+  
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if let index = duplicatePlayers.firstIndex(of: player) {
+            duplicatePlayers.remove(at: index)
         }
+  
     }
-
 
 }
 
